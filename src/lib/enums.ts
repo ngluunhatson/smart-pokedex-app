6af601b8447b19d0ffd81cabd426965b5d1f18e3
Son Nguyen <ngluunhatson@gmail.com>
2025-07-07 01:56:32 +0700

    add favicon and clerk

---
+++ src/lib/enums.ts
@@ -0,0 +1,22 @@
+export enum ThemeEnum {
+  DEFAULT = "default",
+  RED = "red",
+  BLUE = "blue",
+  GREEN = "green",
+  ORANGE = "orange",
+  ROSE = "rose",
+  YELLOW = "yellow",
+  VIOLET = "violet",
+  DEFAULT_DARK = "default-dark",
+  RED_DARK = "red-dark",
+  BLUE_DARK = "blue-dark",
+  GREEN_DARK = "green-dark",
+  ORANGE_DARK = "orange-dark",
+  ROSE_DARK = "rose-dark",
+  YELLOW_DARK = "yellow-dark",
+  VIOLET_DARK = "violet-dark",
+}
+
+export enum LocalStorageKeyEnum {
+  THEME_KEY = "smart-pokedex-theme",
+}
